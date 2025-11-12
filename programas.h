33caceba89a1fc8d47aa661117b00bcb8a2b1a3f
joao-seabra <joao.seabra@aluno.ufop.edu.br>
2025-11-11 22:49:19 -0300

    update

--- programas.h
+++ programas.h
@@ -9,20 +9,27 @@
 
 int programaMulti(CPU *cpu, int a, int b);
 int programaDiv(CPU *cpu, int dividendo, int divisor);
+int programaModulo(CPU *cpu, int a, int b);
 int programaRaizAproximada(CPU *cpu, int valor);
+int programaRaizCubicaAroximada(CPU *cpu, int valor);
 int programaFatorial(CPU *cpu, int valor);
 int programaExponencial(CPU *cpu, int base, int expoente);
-int programaBhaskara(CPU *cpu, int a, int b, int c, int *res);
 int programaLog(CPU *cpu, int base, int logaritmando);
+int programaMdc(CPU *cpu, int a, int b);
+int programaMmc(CPU *cpu, int a, int b);
+int programaBhaskara(CPU *cpu, int a, int b, int c, int *res);
+bool programaEhNumeroPrimo(CPU *cpu, int valor);
+bool ehPar(CPU *cpu, int a);
+
+
 RAM *programaPreencheMatriz(CPU *cpu, RAM *ram, int pontoDePartida, int n, int m);
 RAM *programaMultiplicaMatriz(CPU *cpu, RAM *ram, int n1, int m1, int n2, int m2);
 void imprimirMatriz(CPU *cpu, RAM *ram, int pontoDePartida, int n, int m);
 RAM *criaMatriz(int n, int m, RAM *ram);
-int programaRaizCubicaAroximada(CPU *cpu, int valor);
+
+
 RAM *programaFibonacci(CPU *cpu, RAM *ram,int n);
 int programaTermoPA(CPU *cpu, int a1, int n, int r);
 int programaSomaTermosPA(CPU *cpu, int a1, int n, int r);
-int programaModulo(CPU *cpu, int a, int b);
-bool programaEhNumeroPrimo(CPU *cpu, int valor);
 
 #endif
